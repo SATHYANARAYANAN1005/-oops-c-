998689838dd55e13c19cb43888c0b81abde3deca
SATHYANARAYANAN1005 <148960815+SATHYANARAYANAN1005@users.noreply.github.com>
2024-01-09 12:51:56 +0530

    Create avg 3 numbers.cpp

---
+++ avg 3 numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double a,b,c;
+	cout <<"Enter 3 numbers: ";
+	cin >> a >> b >> c;
+	double avg;
+	avg= (a+b+c)/3.0;
+	cout <<"The average is: "<< avg;
+}
